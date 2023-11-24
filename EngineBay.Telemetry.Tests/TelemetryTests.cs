e18e2cd02351059d4242558dfdd9ad9aaf315032
windy-worm <31342528+windy-worm@users.noreply.github.com>
2023-11-24 09:04:33 +0000

    #patch. update to dotnet8 (#9)
    Co-authored-by: Janus Liebenberg <janus.liebenberg@entelect.co.uk>

---
+++ EngineBay.Telemetry.Tests/TelemetryTests.cs
@@ -0,0 +1,13 @@
+namespace EngineBay.Telemetry.Tests
+{
+    using Xunit;
+
+    public class TelemetryTests
+    {
+        [Fact]
+        public void ControlTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
