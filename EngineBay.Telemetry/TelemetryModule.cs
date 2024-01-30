8f9b0ff5d03fed9b9576e1f511403ca3886122bd
Greatmar2 <greatmar2@gmail.com>
2024-01-30 10:30:53 +0000

    #minor Update Telemetry modules and add Grafana dashboard (#13)
    * Update telemetry dependencies
    
    * Add handler logs and set up grafana dashboard for telemetry
    
    * Improve dashboard
    
    * Allow prerelease packages because we need prometheus exporter

--- EngineBay.Telemetry/TelemetryModule.cs
+++ EngineBay.Telemetry/TelemetryModule.cs
@@ -3,24 +3,20 @@ namespace EngineBay.Telemetry
     using EngineBay.Core;
     using OpenTelemetry.Metrics;
     using OpenTelemetry.Trace;
-    using Proto.OpenTelemetry;
 
     public class TelemetryModule : BaseModule
     {
         public override IServiceCollection RegisterModule(IServiceCollection services, IConfiguration configuration)
         {
-#pragma warning disable CS0618 //OpenTelemetryServicesExtensions.AddOpenTelemetryMetrics is marked as obsolete. Need to refactor this configuration to align to the new interface.
-
-            services.AddOpenTelemetryMetrics((builder) =>
-            {
-                builder
-                .AddProtoActorInstrumentation()
-                .AddAspNetCoreInstrumentation()
-                .AddHttpClientInstrumentation()
-                .AddPrometheusExporter();
-            });
-
-#pragma warning restore CS0618
+            services.AddOpenTelemetry()
+                .WithMetrics(metrics =>
+                {
+                    metrics
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddPrometheusExporter()
+                    .AddConsoleExporter();
+                });
 
             return services;
         }
